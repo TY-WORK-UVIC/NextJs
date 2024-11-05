0af37f28b299db116140d458ef2db1cc03708462
TY <105994651+TY-WORK-UVIC@users.noreply.github.com>
2024-11-05 12:49:31 -0800

    Update page.tsx

--- app/(hero)/performance/page.tsx
+++ app/(hero)/performance/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from '@/components/hero';
-import performanceSrc from '/public/performance.jpg';
+import performanceSrc from '/public/Performance.jpg';
 import { Metadata } from "next";
 export const metadata: Metadata={
   title: 'Performance',
